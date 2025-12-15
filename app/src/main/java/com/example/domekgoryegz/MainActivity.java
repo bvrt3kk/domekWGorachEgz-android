502387e351a2951ffe7093e59c310bf2e2cff5a3
bvrt3kk <danusek12345@gmail.com>
2025-12-15 14:52:52 +0100

    dzialanie

--- app/src/main/java/com/example/domekgoryegz/MainActivity.java
+++ app/src/main/java/com/example/domekgoryegz/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.domekgoryegz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView polubieniaText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        polubieniaText = findViewById(R.id.polubieniaText);
+    }
+    int licznikPolubien = 0;
+
+    public void polub(View view) {
+        licznikPolubien++;
+        polubieniaText.setText(licznikPolubien + " polubień");
+
+    }
+
+    public void usunPolubienie(View view) {
+        if(licznikPolubien > 0){
+            licznikPolubien--;
+        }
+        polubieniaText.setText(licznikPolubien +  " polubień");
     }
 }
